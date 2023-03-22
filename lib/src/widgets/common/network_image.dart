8dd8d08003c470d10d2ad42fc1101891fa3122c2
Finn Nguyen <finn.goldenowl@gmail.com>
2023-03-22 12:05:05 +0700

    Fix network image render error when loading

--- lib/src/widgets/common/network_image.dart
+++ lib/src/widgets/common/network_image.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:shimmer/shimmer.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 import '../../themes/colors.dart';
 
@@ -20,23 +21,29 @@ class LNetworkImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final loading = Container(
+      height: double.infinity,
+      width: double.infinity,
+      color: LColors.loading,
+    ).animate().shimmer();
+    const error = FlutterLogo();
     return SizedBox.square(
       dimension: dimension,
       child: ClipRRect(
         borderRadius: borderRadius,
-        child: CachedNetworkImage(
-          imageUrl: url,
-          fit: fit,
-          progressIndicatorBuilder: (_, __, ___) => Shimmer(
-            gradient: LColors.shimmerGradient,
-            child: Container(
-              height: double.infinity,
-              width: double.infinity,
-              color: LColors.loading,
-            ),
-          ),
-          errorWidget: (context, url, error) => const FlutterLogo(),
-        ),
+        child: kIsWeb
+            ? Image.network(
+                url,
+                fit: fit,
+                loadingBuilder: (_, __, ___) => loading,
+                errorBuilder: (_, __, ___) => error,
+              )
+            : CachedNetworkImage(
+                imageUrl: url,
+                fit: fit,
+                progressIndicatorBuilder: (_, __, ___) => loading,
+                errorWidget: (_, __, ___) => error,
+              ),
       ),
     );
   }
